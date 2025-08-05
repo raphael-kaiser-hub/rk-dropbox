5693968cac5156e050e2ea41ae5ceeda1628711a
raphael-kaiser-hub <raphael-kaiser@gmx.net>
2025-08-05 13:03:23 +0200

    Update dropbox_access.py

--- dropbox_access.py
+++ dropbox_access.py
@@ -5,7 +5,12 @@ import io
 
 DROPBOX_TOKEN = os.getenv("DROPBOX_TOKEN")
 
+ALLOWED_FOLDER = "/Raphael Kaiser/RK IMMOBILIEN/LEB-Leistungserbringung/7- Kundenordner/LEB7-08SN - Schadensregulierung NRW/00_Rechnungen, Hilfsmittel, Vorlagen, etc/App, RC NRW"
+
 def get_pdf_text(path):
+    if not path.startswith(ALLOWED_FOLDER):
+        raise Exception("Zugriff verweigert: Nur Zugriff auf den definierten Ordner erlaubt.")
+
     dbx = dropbox.Dropbox(DROPBOX_TOKEN)
     _, res = dbx.files_download(path)
     pdf_file = io.BytesIO(res.content)
